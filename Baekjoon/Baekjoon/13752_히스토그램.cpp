db6869a6a86a667a05960f84ba3af45653caa530
Ahn honghyun <32119804+hyunynim@users.noreply.github.com>
2018-11-28 18:19:58 +0900

    Create 13752_히스토그램.cpp

---
+++ Baekjoon/Baekjoon/13752_히스토그램.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+  int t;
+  scanf("%d", &t);
+  while(t--){
+    int num;
+    scanf("%d", &num);
+    for(int i = 0; i<num; ++i)
+      printf("=");
+    puts("");
+  }
+}
